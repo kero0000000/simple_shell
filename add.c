7c6d93b5a86b0653a3f337e4d2a2bb306ce0b26e
“kero0000000” <kerorizk27@gmail.com>
2024-02-24 10:28:53 -0800

    simple_shell

---
+++ add.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<unistd.h>
+int main(void)
+{
+int a = 3;
+int b = 5;
+int sum = a + b;
+pid _tpid;
+pid = getpid();
+printf("sum is % d /n", sum);
+printf("pid is % u /n", pid);
+return (0);
+}
---
+++ fork.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<unistd.h>
+int main(void)
+{
+pid_t pid;
+printf("Before fork i was one/n");
+fork();
+if (pid == -1)
+{
+perror("unsuccessful/n");
+return (1);
+}
+printf("After fork i become two/n");
+return (0);
+}
