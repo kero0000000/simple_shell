07e6cd41041b011574e8ef680a47363d8b31513e
“kero0000000” <kerorizk27@gmail.com>
2024-02-27 17:28:33 -0800

    getline.c

--- getline.c
+++ getline.c
@@ -1,2 +1,17 @@
 #include<stdio.h>
-
+#include<stdlib.h>
+/**
+ * main - main
+ * entry type
+ * Return: void
+ **/
+int main(void)
+{
+size_t = 10;
+char *buf = malloc(sizeof(char) * n);
+printf("Enter name");
+getline(&buf, &n, stdin);
+printf("Name is %sBuffer size is %ld\n", buf, n);
+free(buf);
+return (0);
+}
