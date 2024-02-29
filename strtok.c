d748356f889b49b893756b69fd16782ed936d292
“kero0000000” <kerorizk27@gmail.com>
2024-02-28 18:48:46 -0800

    strtok.c

--- strtok.c
+++ strtok.c
@@ -1,5 +1,10 @@
 #include<stdio.h>
 #include<string.h>
+/**
+ * strtok - function to tokenize a string
+ * main - main entry type
+ * Return :void
+ **/
 int main(void)
 {
 char str[] = "jesus loves you";
