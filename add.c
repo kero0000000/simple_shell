a6b9c4e5d6d3467e0d9a733f7e7c3c755486ebf5
“kero0000000” <kerorizk27@gmail.com>
2024-03-16 07:47:01 -0700

    edit add.c

--- add.c
+++ add.c
@@ -12,7 +12,7 @@ int b = 5;
 int sum = a + b;
 pid_t pid;
 pid = getpid();
-printf("sum is % d \n", sum);
-printf("pid is % u \n", pid);
+printf("sum is %d \n", sum);
+printf("pid is %u \n", pid);
 return (0);
 }
